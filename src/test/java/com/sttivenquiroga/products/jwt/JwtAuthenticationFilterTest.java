a5dff086fc33d783a3501b2d6b352374793bd135
Johan <johan.quiroga@cun.edu.co>
2024-06-17 00:27:36 -0500

    Update JwtAuthenticationFilterTest

--- src/test/java/com/sttivenquiroga/products/jwt/JwtAuthenticationFilterTest.java
+++ src/test/java/com/sttivenquiroga/products/jwt/JwtAuthenticationFilterTest.java
@@ -1,35 +1,31 @@
 package com.sttivenquiroga.products.jwt;
 
-import com.fasterxml.jackson.core.util.ByteArrayBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sttivenquiroga.products.entity.UserEntity;
 import com.sttivenquiroga.products.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.catalina.connector.CoyoteInputStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.mock.web.DelegatingServletInputStream;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashSet;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 public class JwtAuthenticationFilterTest {
@@ -64,7 +60,6 @@ public class JwtAuthenticationFilterTest {
     @Test
     public void testAuthentication() throws ServletException, IOException {
         String token = "tokenDePrueba";
-        UserDetails userDetails = mock(UserDetails.class);
         User user = mock(User.class);
         UserEntity userEntity = new UserEntity(1, "user", "juan", "perez", "12345", new HashSet<>());
         ObjectMapper objectMapper = new ObjectMapper();
@@ -78,5 +73,21 @@ public class JwtAuthenticationFilterTest {
 
         jwtAuthenticationFilter.attemptAuthentication(request, response);
         jwtAuthenticationFilter.successfulAuthentication(request, response, filterChain, authenticationResult);
+    }
+
+    @Test
+    public void testAuthenticationExeption() throws IOException {
+        String token = "tokenDePrueba";
+        User user = mock(User.class);
+        UserEntity userEntity = new UserEntity(1, "user", "juan", "perez", "12345", new HashSet<>());
+        ObjectMapper objectMapper = new ObjectMapper();
+        byte[] jsonData = objectMapper.writeValueAsBytes(userEntity);
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.setContent(jsonData);
+        when(request.getInputStream()).thenThrow(IOException.class);
+        when(authenticationManager.authenticate(any())).thenReturn(authenticationResult);
+        when(authenticationResult.getPrincipal()).thenReturn(user);
+        when(jwtService.generateToken(authenticationResult.getName())).thenReturn(token);
+        assertThrows(AuthenticationServiceException.class, () ->  jwtAuthenticationFilter.attemptAuthentication(request, response));
     }
 }
