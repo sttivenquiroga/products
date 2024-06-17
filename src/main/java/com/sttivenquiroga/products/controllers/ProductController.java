d91452afdd8253097cd6eb8236584839dda8835c
Johan Quiroga <sttivenquiroga@gmail.com>
2024-06-17 17:09:50 -0500

    Update controller to set correct advice per endpoint

--- src/main/java/com/sttivenquiroga/products/controllers/ProductController.java
+++ src/main/java/com/sttivenquiroga/products/controllers/ProductController.java
@@ -32,7 +32,13 @@ public class ProductController {
     @GetMapping("products/{productId}")
     public ResponseEntity<ProductDTO> getProductById(@PathVariable Integer productId) {
         ProductDTO product = productMapper.mapProductToProductDTO(productService.getProductById(productId));
-        return ResponseEntity.ok(product);
+        ResponseEntity response;
+        if (product != null) {
+            response = ResponseEntity.ok(product);
+        } else {
+            response = ResponseEntity.noContent().build();
+        }
+        return response;
     }
 
     @PreAuthorize("hasRole('ADMIN')")
@@ -42,22 +48,33 @@ public class ProductController {
         if (updatedProduct != null) {
             return ResponseEntity.ok(updatedProduct);
         } else {
-            return ResponseEntity.badRequest().body(ResponseMessage.builder().message("No se puedo actualizar el producto").build());
+            return ResponseEntity.badRequest().body(ResponseMessage.builder().message("No se pudo actualizar el producto").build());
         }
     }
 
     @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping("products")
-    public ResponseEntity<List<ProductDTO>> getProductByCategory(@RequestParam String categoryName){
-        List<ProductDTO> productDTOList = productMapper.mapProductListToProductDtoList(productService.getProductByCategory(categoryName));
+    public ResponseEntity<List<ProductDTO>> getProductByCategory(@RequestParam String categoryName) {
+        List<ProductDTO> productDTOList;
+        if (categoryName.equalsIgnoreCase("all")) {
+            productDTOList = productMapper.mapProductListToProductDtoList(productService.getAllProducts());
+        } else {
+            productDTOList = productMapper.mapProductListToProductDtoList(productService.getProductByCategory(categoryName));
+        }
         return ResponseEntity.ok(productDTOList);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("products/{productId}")
-    public ResponseEntity<ResponseMessage> deleteProduct(@PathVariable Integer productId){
-        productService.deleteProductById(productId);
-        return ResponseEntity.ok(ResponseMessage.builder().message("Producto eliminado con exito").build());
+    public ResponseEntity<ResponseMessage> deleteProduct(@PathVariable Integer productId) {
+        boolean deleted = productService.deleteProductById(productId);
+        ResponseEntity response;
+        if (deleted) {
+            response = ResponseEntity.ok(ResponseMessage.builder().message("Producto eliminado con exito").build());
+        } else {
+            response = ResponseEntity.badRequest().body(ResponseMessage.builder().message("El producto no pudo ser eliminado").build());
+        }
+        return response;
     }
 
 }
